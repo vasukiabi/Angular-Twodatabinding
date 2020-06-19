9719c1b847ccbec9ba22e1788871111c4b764ccb
Vasuki <vasukiabi@gmail.com>
2020-06-19 16:18:57 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` 
+  Enter the value  : <input [(ngModel)] ='val'> 
+  <br> 
+   Entered value is:  {{val}} 
+<br>
+   <input [ngModel]="username" (ngModelChange)="username = $event">
+<br>
+<p>Hello {{username}}!</p>
+` 
+
+})
+export class AppComponent {
+  title = 'TwoBinding';
+  val: string = 'India'; 
+  username:any='muruga';
+
+}
